23b4bbdfcfcf4ba5168ab73971bac75379feb858
Silvia-1627 <Silviasingh270@gmail.com>
2026-07-09 00:10:28 +0530

    Added resume upload functionality with multer

--- src/pages/ResumeUpload.jsx
+++ src/pages/ResumeUpload.jsx
@@ -1,9 +1,40 @@
 import { useState } from "react";
+import axios from "axios";
 import "./ResumeUpload.css";
 
 function ResumeUpload() {
   const [resume, setResume] = useState(null);
   const [showAnalysis, setShowAnalysis] = useState(false);
+  const [uploadMessage, setUploadMessage] = useState("");
+  const handleUpload = async () => {
+
+  if (!resume) {
+    alert("Please select a resume first.");
+    return;
+  }
+
+  const formData = new FormData();
+
+  formData.append("resume", resume);
+
+  try {
+
+    const response = await axios.post(
+      "http://localhost:5000/upload",
+      formData
+    );
+
+    setUploadMessage(response.data.message);
+
+  } catch (error) {
+
+    console.log(error);
+
+    setUploadMessage("Upload failed.");
+
+  }
+
+};
   const fileSize =
   resume
     ? (resume.size / 1024).toFixed(2)
@@ -16,6 +47,11 @@ function ResumeUpload() {
   return (
     <div className="resume-container">
       <h1>Upload Resume</h1>
+      {uploadMessage && (
+  <p className="upload-message">
+    {uploadMessage}
+  </p>
+)}
 
       <input
         type="file"
@@ -38,11 +74,11 @@ function ResumeUpload() {
    <p className="analysis-ready">
     Ready For AI Analysis
    </p>
-   <button
+     <button
   className="analyze-btn"
-  onClick={() => setShowAnalysis(true)}
+  onClick={handleUpload}
 >
-  Analyze Resume
+  Upload Resume
 </button>
 {showAnalysis && (
   <div className="analysis-box">
@@ -58,12 +94,12 @@ function ResumeUpload() {
 
     <h3>Suggested Interview Questions</h3>
 
-<ul>
-  <li>Explain Java OOP concepts.</li>
-  <li>What is React Virtual DOM?</li>
-  <li>How do arrays work in DSA?</li>
-  <li>Describe a challenging project you built.</li>
-</ul>
+    <ul>
+    <li>Explain Java OOP concepts.</li>
+    <li>What is React Virtual DOM?</li>
+    <li>How do arrays work in DSA?</li>
+    <li>Describe a challenging project you built.</li>
+   </ul>
 
   </div>
 )}
